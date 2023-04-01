221f1963f4256e44475c9e2640a3ceecbf55032c
User <User@DESKTOP-ROOTJT0>
2023-04-01 14:54:26 +0200

    Add project files.

---
+++ EqualityOfVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EqualityOfVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+            Console.WriteLine("Program to check parity of our variables");
+            Console.WriteLine("a = 5");
+            Console.WriteLine("b = 5");
+            if (a == b)
+            {
+                Console.WriteLine("Variables a and b are equal");
+            }
+            else
+            {
+                Console.WriteLine("Variables a and b are not equal");
+            }
+        }
+    }
+
+}
